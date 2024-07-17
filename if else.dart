b72d5ca25247eb88322eebb2029bac002e39c0fd
Hafiz Saqlain Ur Rehman <114049185+Hafizsaqlain@users.noreply.github.com>
2024-07-17 12:18:47 +0500

    Add files via upload

---
+++ if else.dart
@@ -0,0 +1,14 @@
+void main(){
+
+    int number=0;
+    if(number>0){
+    print ('The number is negative.');
+    } else if (number < 0){
+        print ('The number is zero.');
+    } else{
+        print ('The number is zero.');
+     }
+
+
+
+}
